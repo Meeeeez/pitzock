f659dab29794e87769658238581c7ec6b2d2b279
Moritz Messner <moritzmessner122@gmail.com>
2025-11-29 14:21:42 +0100

    added additional reservation date validation

---
+++ pb/pb_hooks/main.pb.js
@@ -0,0 +1,4 @@
+/// <reference path="..\pb_data\types.d.ts" />
+
+// reservations
+require(`${__hooks}/reservations/onRecordCreateRequest.js`);
---
+++ pb/pb_hooks/reservations/onRecordCreateRequest.js
@@ -0,0 +1,17 @@
+onRecordCreateRequest((e) => {
+    const body = e.requestInfo().body
+
+    const now = new Date()
+    const startDateTime = new Date(body.startsAt)
+    const endDateTime = new Date(body.endsAt)
+
+    if (startDateTime < now) {
+        throw new BadRequestError("Reservation date must be in the future")
+    }
+
+    if (endDateTime <= startDateTime) {
+        throw new BadRequestError("Reservation end date must be after start date")
+    }
+
+    e.next()
+}, "reservations")
