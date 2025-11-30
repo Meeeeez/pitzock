45ecf0ef4fb024d44ffde85000055a22445601be
Moritz Messner <moritzmessner122@gmail.com>
2025-11-30 23:01:55 +0100

    built sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-import { Routes } from "react-router";
+import { Route, Routes } from "react-router";
+import { Dashboard } from "./pages/Dashboard";
+import { RootLayout } from "./layouts/RootLayout";
 
 export default function App() {
-  return <Routes></Routes>;
+  return (
+    <Routes>
+      <Route element={<RootLayout />}>
+        <Route index element={<Dashboard />} />
+      </Route>
+    </Routes>
+  );
 }
---
+++ src/components/ui/app-sidebar.tsx
@@ -0,0 +1,74 @@
+
+
+import * as React from "react"
+import {
+  Command,
+} from "lucide-react"
+
+import { NavUser } from "@/components/ui/nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import { Calendar } from "@/components/ui/calendar"
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  }
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const [date, setDate] = React.useState<Date | undefined>(new Date())
+
+  return (
+    <Sidebar variant="sidebar" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                  <Command className="size-4" />
+                </div>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">Acme Inc</span>
+                  <span className="truncate text-xs">Enterprise</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <Separator className="my-1" />
+      <SidebarContent className="justify-between px-2 py-4">
+        <div>
+          <Button onClick={() => setDate(new Date())} variant="outline" className="w-full">Today</Button>
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+            className="w-full bg-neutral"
+          />
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <Button variant="outline">Walk in</Button>
+          <Button>New Booking</Button>
+        </div>
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
