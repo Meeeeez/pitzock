c3f85d6570b0c57789bff922885c8f0bf2247238
Moritz Messner <moritzmessner122@gmail.com>
2025-12-02 10:49:23 +0100

    added react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import { Route, Routes } from "react-router";
 import { Dashboard } from "./pages/Dashboard";
 import { RootLayout } from "./layouts/RootLayout";
 
 export default function App() {
+  const queryClient = new QueryClient()
   return (
-    <Routes>
-      <Route element={<RootLayout />}>
-        <Route index element={<Dashboard />} />
-      </Route>
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route element={<RootLayout />}>
+          <Route index element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </QueryClientProvider>
   );
 }
