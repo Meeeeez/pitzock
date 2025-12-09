04738a9b1ae659cbc6272bb9b08b16ac54bc400e
Moritz Messner <moritzmessner122@gmail.com>
2025-12-09 17:48:09 +0100

    fixed small issue where we throw our api errors inside the try block

--- pb/pb_hooks/clients/onRecordCreateRequest.js
+++ pb/pb_hooks/clients/onRecordCreateRequest.js
@@ -4,20 +4,19 @@ onRecordCreateRequest((e) => {
   const body = e.requestInfo().body
 
   // check if a client with this email already exists for this business
+  const existingClient = new DynamicModel({ "id": "" })
   try {
-    const existingClient = new DynamicModel({ "id": "" })
-
     e.app.db()
       .select("id")
       .from("clients")
       .where($dbx.like("email", body.email))
       .andWhere($dbx.like("businessId", body.businessId))
       .one(existingClient);
-
-    if (existingClient.id) {
-      throw new BadRequestError("A client with this email already exists for this business!");
-    }
   } catch (e) { }
+
+  if (existingClient.id) {
+    throw new BadRequestError("A client with this email already exists for this business!");
+  }
 
   e.next()
 }, "clients")
