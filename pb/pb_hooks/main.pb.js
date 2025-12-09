258265bc5dc2528fc25b0fea0d36b392a3513691
Moritz Messner <moritzmessner122@gmail.com>
2025-12-09 23:29:59 +0100

    onRecordAfterCreateSuccess on reservation bootstrap

--- pb/pb_hooks/main.pb.js
+++ pb/pb_hooks/main.pb.js
@@ -2,11 +2,10 @@
 
 // reservations
 require(`${__hooks}/reservations/onRecordCreateRequest.js`);
+require(`${__hooks}/reservations/onRecordAfterCreateSuccess.js`);
 
 // clients
 require(`${__hooks}/clients/onRecordCreateRequest.js`);
 
 // station merges
 require(`${__hooks}/station-merges/onRecordCreateRequest.js`);
-
-
