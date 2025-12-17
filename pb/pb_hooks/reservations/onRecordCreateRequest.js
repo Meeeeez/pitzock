2e50646a69f85dafe8e4c9e5abd91160b87b8c27
Moritz Messner <moritzmessner122@gmail.com>
2025-12-17 15:24:53 +0100

    added more validation to reservationOnRecordCreateRequest

--- pb/pb_hooks/reservations/onRecordCreateRequest.js
+++ pb/pb_hooks/reservations/onRecordCreateRequest.js
@@ -1,19 +1,31 @@
 /// <reference path="..\..\pb_data\types.d.ts" />
 
 onRecordCreateRequest((e) => {
-  const body = e.requestInfo().body
+  const utils = require(`${__hooks}/reservations/utils.js`)
 
   const now = new Date()
-  const startDateTime = new Date(body.startsAt)
-  const endDateTime = new Date(body.endsAt)
+  const endsAt = new Date(e.record.getString("endsAt"))
+  const startsAt = new Date(e.record.getString("startsAt"))
+  const business = utils.getBusinessById(e.auth.id)
 
-  if (startDateTime < now) {
-    throw new BadRequestError("Reservation date must be in the future")
-  }
+  // 1. check if start is in the future
+  if (startsAt <= now) throw new BadRequestError("Reservation start must be in the future")
 
-  if (endDateTime <= startDateTime) {
-    throw new BadRequestError("Reservation end date must be after start date")
-  }
+  // 2. check if endsAt is after startsAt
+  if (endsAt <= startsAt) throw new BadRequestError("Reservation end must be after reservation start")
+
+  // 3. check if business is active
+  if (!business.getBool("isActive")) throw new BadRequestError("Business is inactive")
+
+  // 4. check if business is on holiday
+  const businessId = business.getString("id")
+  const overlapsWithHoliday = utils.isReservationOverlappingWithBusinessHoliday(businessId, e.record)
+  if (overlapsWithHoliday) throw new BadRequestError("Business is on holiday")
+
+  // 3. check if business is open (opening hours)
+  const businessOpeningHours = JSON.parse(business.getString("openingHours"))
+  const isWithinOpeningHours = utils.isReservationWithinOpeningHours(businessOpeningHours, e.record)
+  if (!isWithinOpeningHours) throw new BadRequestError("Business is closed")
 
   e.next()
 }, "reservations")
